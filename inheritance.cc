1cf790b9449ea94590e999a1b94279c4a10f3443
RajatMehar <92201235+RajatMehar@users.noreply.github.com>
2021-11-22 16:48:10 +0530

    Add files via upload

---
+++ inheritance.cc
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+class B {
+int a;
+public:
+int b;
+void get_ab();
+int get_a();
+void show_a();
+};
+ 
+class D:public B
+{
+	int c;
+	public:
+void mul();
+void display();
+
+};
+
+void B :: get_ab(void)
+{
+	a=5;
+	b=10;
+}
+
+int B::get_a()
+{
+	return a;
+}
+
+void B :: show_a()
+{
+	cout<< "a ="<< a << endl;
+}
+
+void D :: mul(void)
+{
+	c=b*get_a();
+}
+
+void D :: display()
+{
+	cout<< "a="<<get_a();
+	cout<< "b="<<b;
+	cout<< "c="<<c;
+}
+
+int main()
+{
+	D d;
+d.get_ab();
+d.mul();
+d.show_a();
+d.display();
+d.b=20;
+d.mul();
+d.display();
+return 0;
+}
